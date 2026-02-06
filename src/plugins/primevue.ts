4a9ffc7ddf01dffcede2a0ca8cef286251fffbb7
Adrian Mazur <adrianmazur.dev@proton.me>
2026-02-06 15:20:03 +0100

    fix: darkModeSelector to false to fix styles

--- src/plugins/primevue.ts
+++ src/plugins/primevue.ts
@@ -10,6 +10,7 @@ export function setupPrimeVue(app: App) {
             preset: Aura,
             options: {
                 prefix: 'p',
+                darkModeSelector: false,
             },
         },
     })
